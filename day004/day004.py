de344b817dffd398aa881894d4b5f89fc9d917a0
nikachitadze2 <nikolozichitadze107@gmail.com>
2025-03-14 16:42:06 +0400

    first commit

---
+++ day004/day004.py
@@ -0,0 +1,16 @@
+age = "15"
+age1 = "25"
+print(age + age1)
+
+
+print(900 // 3)
+print(2000 // 5)
+
+print(5.5 * 10.5)
+print(20 * 30)
+
+print(50.5 - 30.5)
+print(100 - 50)
+
+print(30 + 60)
+print(2.5 + 2.5)
---
+++ day006/index.py
@@ -0,0 +1,4 @@
+points = "50"
+points = "25"
+points = "70"
+print(points)
