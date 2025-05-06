1d407129223c5fbf6c26e328d23d1ab991a1fd0a
nikachitadze2 <nikolozichitadze107@gmail.com>
2025-05-06 19:12:29 +0400

    third commit

---
+++ day002/homework/day002.py
@@ -0,0 +1,13 @@
+print(40.5-20.5)
+
+print(15*4)
+print(20*5)
+print(20*5)
+
+
+name="chemisaxelianika"
+surname="chitsdze"
+age="mevar15wlis"
+hight="simagleshivar1.77"
+
+print(name+""+surname+""+age+""+hight)
---
+++ day003/homework/day003.py
@@ -0,0 +1,15 @@
+# str1="string1"
+# str2="string2"
+# str3="string3"
+
+# int1=5
+# int2=10
+# in3=20
+
+# float1="1.5"
+# float2="2.5"
+# float3="3.5"
+
+# print(str1,str2,str3)
+# print(int1,int2,int3)
+# print(float1,float2,float3)
