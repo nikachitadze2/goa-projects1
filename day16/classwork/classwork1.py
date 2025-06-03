66f25d588118198073210c73d80dec7c371862e8
nikachitadze2 <nikolozichitadze107@gmail.com>
2025-06-03 23:00:00 +0400

    second commit

---
+++ day16/classwork/classwork1.py
@@ -0,0 +1,5 @@
+password = "nika"
+user_password = input("Enter your password")
+while user_password != password:
+    user_password = input("Enter your password")
+print("corect")
---
+++ day16/classwork/classwork2.py
@@ -0,0 +1,6 @@
+num = 1
+
+while num < 21:
+    print(num)
+    num = num + 1
+
